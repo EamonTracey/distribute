7dd535ddb391f4b9d17bdf9d980aee85287f4325
Eamon Tracey <etracey@student11.cse.nd.edu>
2024-10-04 08:40:35 -0400

    complete assignment4

--- assignment4/TestOutliers.py
+++ assignment4/TestOutliers.py
@@ -23,6 +23,24 @@ def main(args: argparse.Namespace):
             end = time.time()
             insert_times.append(end - start)
 
+    # Test performance of multiple lookups individually.
+    lookup_times = []
+    for i in range(1, SIZE + 1):
+        for j in range(1, SIZE + 1):
+            start = time.time()
+            spreadsheet.lookup(i, j)
+            end = time.time()
+            lookup_times.append(end - start)
+
+    # Test performance of multiple querys individually.
+    query_times = []
+    for i in range(1, SIZE + 1):
+        for j in range(1, SIZE + 1):
+            start = time.time()
+            spreadsheet.query(1, 1, SIZE, SIZE)
+            end = time.time()
+            query_times.append(end - start)
+
     # Test performance of multiple removes individually.
     remove_times = []
     for i in range(1, SIZE + 1):
@@ -32,16 +50,40 @@ def main(args: argparse.Namespace):
             end = time.time()
             remove_times.append(end - start)
 
+    # Test performance of multiple sizes individually.
+    size_times = []
+    for i in range(1, SIZE + 1):
+        for j in range(1, SIZE + 1):
+            start = time.time()
+            spreadsheet.size()
+            end = time.time()
+            size_times.append(end - start)
+
     n_operations = SIZE * SIZE
     print(f"Insert ({n_operations})")
     print(f"    Minimum Time (s): {min(insert_times)}")
     print(f"    Maximum Time (s): {max(insert_times)}")
     print(f"    Average Time (s): {sum(insert_times) / len(insert_times)}")
 
+    print(f"Lookup ({n_operations})")
+    print(f"    Minimum Time (s): {min(lookup_times)}")
+    print(f"    Maximum Time (s): {max(lookup_times)}")
+    print(f"    Average Time (s): {sum(lookup_times) / len(lookup_times)}")
+
+    print(f"Query ({n_operations})")
+    print(f"    Minimum Time (s): {min(query_times)}")
+    print(f"    Maximum Time (s): {max(query_times)}")
+    print(f"    Average Time (s): {sum(query_times) / len(query_times)}")
+
     print(f"Remove ({n_operations})")
     print(f"    Minimum Time (s): {min(remove_times)}")
     print(f"    Maximum Time (s): {max(remove_times)}")
     print(f"    Average Time (s): {sum(remove_times) / len(remove_times)}")
+
+    print(f"Size ({n_operations})")
+    print(f"    Minimum Time (s): {min(size_times)}")
+    print(f"    Maximum Time (s): {max(size_times)}")
+    print(f"    Average Time (s): {sum(size_times) / len(size_times)}")
 
 
 if __name__ == "__main__":
