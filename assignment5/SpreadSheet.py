f893db47c837bd41a1f7b90f00563beec064cbb7
Eamon Tracey <etracey@student13.cse.nd.edu>
2024-10-10 16:39:47 -0400

    copy assignment 4 to 5

---
+++ assignment5/SpreadSheet.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+
+class SpreadSheet:
+
+    def __init__(self):
+        self._data = dict()
+
+    def insert(self, row: int, col: int, value: int):
+        if row < 1:
+            raise ValueError(f"row {row} cannot be negative")
+        if col < 1:
+            raise ValueError(f"column {col} cannot be negative")
+
+        self._data[(row, col)] = value
+
+    def lookup(self, row: int, col: int) -> Any:
+        if (row, col) not in self._data:
+            raise LookupError(f"no data at row {row} column {col}")
+
+        return self._data[(row, col)]
+
+    def remove(self, row: int, col: int):
+        if row < 1:
+            raise ValueError(f"row {row} cannot be negative")
+        if col < 1:
+            raise ValueError(f"column {col} cannot be negative")
+
+        if (row, col) in self._data:
+            del self._data[(row, col)]
+
+    def query(self, row: int, col: int, width: int,
+              height: int) -> list[list[Any]]:
+        row_max, col_max = self.size()
+
+        if row < 1:
+            raise ValueError(f"row {row} cannot be negative")
+        if row + height - 1 > row_max:
+            raise ValueError(
+                f"row {row + width - 1} exceeds number of rows {row_max}")
+        if col < 1:
+            raise ValueError(f"col {col} cannot be negative")
+        if col + width - 1 > col_max:
+            raise ValueError(
+                f"col {col + height - 1} exceeds number of columns {col_max}")
+
+        values = {}
+        for (r, c) in self._data:
+            if row <= r <= row + height - 1 and col <= c <= col + width - 1:
+                values[(r, c)] = self._data[(r, c)]
+
+        return values
+
+    def size(self) -> tuple[int, int]:
+        row_max = 0
+        col_max = 0
+
+        for (r, c) in self._data:
+            row_max = max(row_max, r)
+            col_max = max(col_max, c)
+
+        return (row_max, col_max)
---
+++ assignment5/SpreadSheetClient.py
@@ -0,0 +1,153 @@
+import builtins
+import json
+import pickle
+import socket
+import urllib.request
+import time
+from typing import Any
+
+NAMESERVER = "http://catalog.cse.nd.edu:9097/query.json"
+
+
+class SpreadSheetClient:
+
+    def __init__(self, name):
+        self.name = name
+
+        self._socket = None
+        self._connect()
+
+    def _rpc(func):
+
+        def wrapper(self, *args, **kwargs):
+            payload = func(self, *args, **kwargs)
+            message = pickle.dumps(payload)
+            while True:
+                try:
+                    self._send_message(message)
+                    response = self._receive_response()
+                    if response is None:
+                        raise ConnectionError
+                    break
+                except Exception as exception:
+                    print(
+                        "Failed to contact server, attempting reconnect now"
+                    )
+                    self._connect()
+
+            # Payload containing exception_name indicates we must raise an exception.
+            payload = pickle.loads(response)
+            if "exception_name" in payload:
+                exception_name = payload["exception_name"]
+                exception_message = payload["exception_message"]
+                raise getattr(builtins, exception_name)(exception_message)
+
+            # Otherwise, return the specified return value.
+            return payload["return"]
+
+        return wrapper
+
+    def _send_message(self, message: bytes):
+        length = len(message).to_bytes(4, byteorder="big")
+        self._socket.sendall(length)
+        self._socket.sendall(message)
+
+    def _receive_response(self) -> bytes:
+        length = self._socket.recv(4)
+        if not length:
+            return None
+
+        length = int.from_bytes(length, byteorder="big")
+        response = bytes()
+        while len(response) < length:
+            response += self._socket.recv(length - len(response))
+        if not response:
+            return None
+
+        return response
+
+    def _connect(self):
+        if self._socket is not None:
+            self._socket.close()
+
+        timeout = 1
+        while True:
+            try:
+                payload: dict
+                with urllib.request.urlopen(NAMESERVER) as response:
+                    payload = json.loads(response.read().decode())
+
+                matches = []
+                for data in payload:
+                    if ("type" in data and "project" in data
+                            and data["type"] == "spreadsheet"
+                            and data["project"] == self.name):
+                        matches.append(data)
+
+                host = matches[0]["address"]
+                port = matches[0]["port"]
+                heard = int(matches[0]["lastheardfrom"])
+                for match in matches:
+                    if match["lastheardfrom"] > heard:
+                        host = match["address"]
+                        port = match["port"]
+                        heard = int(match["lastheardfrom"])
+
+                self._socket = socket.socket(socket.AF_INET,
+                                             socket.SOCK_STREAM)
+                self._socket.settimeout(5)
+                self._socket.connect((host, port))
+                break
+            except Exception as exception:
+                print(
+                    f"Failed to connect to {self.name}, attempting reconnect in {timeout} seconds"
+                )
+                time.sleep(timeout)
+                timeout *= 2
+
+    @_rpc
+    def insert(self, row: int, col: int, value: int):
+        payload = {
+            "function": "insert",
+            "arguments": {
+                "row": row,
+                "col": col,
+                "value": value
+            }
+        }
+        return payload
+
+    @_rpc
+    def lookup(self, row: int, col: int) -> Any:
+        payload = {"function": "lookup", "arguments": {"row": row, "col": col}}
+        return payload
+
+    @_rpc
+    def remove(self, row: int, col: int):
+        payload = {
+            "function": "remove",
+            "arguments": {
+                "row": row,
+                "col": col,
+            }
+        }
+        return payload
+
+    @_rpc
+    def query(self, row: int, col: int, width: int,
+              height: int) -> list[list[int]]:
+        payload = {
+            "function": "query",
+            "arguments": {
+                "row": row,
+                "col": col,
+                "width": width,
+                "height": height
+            }
+        }
+        return payload
+
+    @_rpc
+    def size(self) -> tuple[int, int]:
+        payload = {"function": "size", "arguments": {}}
+        return payload
